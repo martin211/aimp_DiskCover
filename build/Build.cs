dd432a985a3c90753bfc52839b95ec89de046283
martin211 <mail4evgeniy@gmail.com>
2018-11-13 00:32:03 +0300

    Update build project

--- build/Build.cs
+++ build/Build.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using ICSharpCode.SharpZipLib.Zip;
 using Nuke.Common;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
